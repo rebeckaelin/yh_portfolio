74ca7494b8d444e717a2d1e58b77b06db6f65d0f
rebeckaelin <rebeckaelin@hotmail.com>
2025-02-25 16:53:37 +0100

    updated .env

--- portfolio/src/firebase.js
+++ portfolio/src/firebase.js
@@ -8,12 +8,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGsPskH0omHchiN5r3NKq7ypLLe8Hvp_s",
-  authDomain: "heartcounter-15496.firebaseapp.com",
-  projectId: "heartcounter-15496",
-  storageBucket: "heartcounter-15496.firebasestorage.app",
-  messagingSenderId: "868357094083",
-  appId: "1:868357094083:web:83c011288ca8154aaf4076",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
